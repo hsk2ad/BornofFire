a702329798905b60b3be7335f4c04daa743da818
jherd23 <jherd23@gmail.com>
2018-10-22 01:21:04 -0400

    Some initial drabbles

---
+++ Born of Fire/Assets/Script/Dialogue.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Cradle;
+using Cradle.StoryFormats.Harlowe;
+
+public class Dialogue : MonoBehaviour {
+
+    public Story story;
+
+    public GameObject textTemplate;
+    public GameObject linkTemplate;
+    public GameObject linebreakTemplate;
+
+    public GameObject DialogueContainer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Display(StoryOutput output) {
+        GameObject UIchild = GenerateUIElement(output);
+        AppendToConsole(UIchild);
+    }
+     
+    public void DoLink(StoryLink link) {
+
+    }
+
+    GameObject GenerateUIElement(StoryOutput output) {
+        if(output is StoryText) {
+            GameObject newTextElement = Instantiate(textTemplate);
+            Text visibleText = newTextElement.GetComponent<Text>();
+            visibleText.text = output.Text;
+
+            return newTextElement;
+        } else if(output is StoryLink) {
+            GameObject newLinkElement = Instantiate(linkTemplate);
+            Text visibleText = newLinkElement.GetComponent<Text>();
+            visibleText.text = output.Text;
+
+            LinkButton link = newLinkElement.GetComponent<LinkButton>();
+            link.SetDelegate(() => {
+                DoLink((StoryLink)output);
+            });
+
+            return newLinkElement;
+        } else if(output is LineBreak) {
+            GameObject newLinebreakElement = Instantiate(linebreakTemplate);
+
+            return newLinebreakElement;
+        }
+        return null;
+    }
+
+    void AppendToConsole(GameObject uiElement) {
+
+    }
+}
---
+++ Born of Fire/Assets/Script/LinkButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Cradle;
+using Cradle.StoryFormats.Harlowe;
+
+public class LinkButton : MonoBehaviour
+{
+
+    public delegate void ClickFunction();
+
+    public ClickFunction onclick;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetDelegate(ClickFunction del) {
+        onclick = del;
+    }
+
+    private void OnMouseDown()
+    {
+        onclick();
+    }
+}
