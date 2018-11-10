54aba3013643deb86872e72a71615e689540fcd2
jherd23 <jherd23@gmail.com>
2018-11-09 21:20:26 -0500

    Added Main Menu

---
+++ Born of Fire/Assets/Resources/Script/UISceneButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class UISceneButton : MonoBehaviour, IPointerClickHandler {
+
+    public string ToGoTo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerClick(PointerEventData pedata) {
+        Debug.Log("works....");
+        if (pedata.button == PointerEventData.InputButton.Left) {
+
+            SceneTransition.GoToScene(ToGoTo);
+        }
+    }
+}
