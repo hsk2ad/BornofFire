3caac2fc4f2526f54681651bbf6750e490d7dcd4
jherd23 <jherd23@gmail.com>
2018-11-09 20:01:54 -0500

    Big ol' Changes

---
+++ Born of Fire/Assets/Resources/Script/SceneTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class SceneTrigger : MonoBehaviour {
+
+    public string Destination;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown() {
+        SceneTransition.GoToScene(Destination);
+    }
+}
