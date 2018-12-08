e1fe091f9b4c2b47a37a4ecf48389dc23ab90dc5
jherd23 <jherd23@gmail.com>
2018-12-07 23:40:53 -0500

    Premonitions
    omens.

---
+++ Born of Fire/Assets/Resources/Script/CrawlspaceOpener.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cradle;
+
+public class CrawlspaceOpener : MonoBehaviour {
+
+    public GameObject crawlspace;
+
+	// Use this for initialization
+	void Start () {
+        crawlspace.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OPEN() {
+        crawlspace.SetActive(true);
+    }
+}
+
+public class HOOK : RuntimeMacros {
+    [RuntimeMacro]
+    public void OpenCrawlspace(string blah) {
+        this.Story.SendMessage("OPEN");
+    }
+}
