7180a05fa8f31322bf740e73b8fa393272169e95
jherd23 <jherd23@gmail.com>
2018-12-09 05:15:39 -0500

    BGM Added

---
+++ Born of Fire/Assets/Resources/Script/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    public static MusicPlayer instance;
+
+    public AudioSource player;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        } else {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        player.loop = true;
+        player.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
